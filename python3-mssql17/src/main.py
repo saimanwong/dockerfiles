5e104ab21ddf460ad9d09375532bc830cbffe308
Sai Man Wong <saimanwong@gmail.com>
2021-09-15 00:28:34 +0200

    add python3 mssql17 example

---
+++ python3-mssql17/src/main.py
@@ -0,0 +1,19 @@
+# https://docs.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver15
+
+import pyodbc
+# Some other example server values are
+# server = 'localhost\sqlexpress' # for a named instance
+# server = 'myserver,port' # to specify an alternate port
+server = 'mssql'
+database = 'mydb'
+username = 'sa'
+password = 'Test123!!!'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
+#Sample select query
+cursor.execute("SELECT @@version;")
+row = cursor.fetchone()
+while row:
+    print(row[0])
+    row = cursor.fetchone()
